78e6df133edbb0098b03d5230eb882f8714cb050
Mark <mrkcarlos576@gmail.com>
2024-04-05 21:38:41 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+  let message = [];
+
+  for (let i = 0; i < names.length; i++) {
+    let result = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    message.push(result);
+  }
+
+  return message;
+}
+
+const result = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(result);
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+  countDown(10);
